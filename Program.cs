6973b821d9ccaae36d47958e4dc508c051e886a1
Olatunji Yusuf <olatunjiyso@gmail.com>
2024-06-08 17:35:20 +0100

    fix allowed origins

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using QuickTodos.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+ builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
@@ -13,8 +13,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
